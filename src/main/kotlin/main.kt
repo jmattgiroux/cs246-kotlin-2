a5fad343f30455b2f9b0344496ddb40d36046801
Jmattgiroux <git config --global user.email jmattgiroux@gmail.comgit config --global user.email>
2020-12-11 20:22:53 -0600

    Finished implementation, put screenshot in repo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,49 @@
+package pack
+
+import DiceBag
 
 
+fun main () {
+
+    var play = true
+
+    val bag1 = DiceBag()
+    val bag2 = DiceBag()
 
 
+    var response = String()
 
-fun main (args: Array<String>) {
-    println("Hello World")
+
+    while(play){
+
+        println("Your dice")
+        bag1.rollAll()
+
+        println("Computer's dice")
+        bag2.rollAll()
+
+
+        var sum1 = bag1.sum()
+
+
+        var sum2 = bag2.sum()
+
+        println("Your total: $sum1")
+        println("Computer's total: $sum2")
+
+        if (sum1 > sum2)
+            println("You won!")
+        else if (sum2 > sum1)
+            println("You lost!")
+        else
+            println("Tie!")
+
+        print("Would you like to play again? Y/N: ")
+        response = readLine().toString()
+        response.capitalize()
+        if (response == "N")
+            play = false
+    }
+
+
 }
